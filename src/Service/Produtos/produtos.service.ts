326cfc7fedb56ec212d49775be00861e1e8f25ba
Iara Grossert <iara_grossert@hotmail.com>
2022-06-06 22:16:34 -0300

    feat(Produtos): Criadas validações nos endpoints de produtos

--- src/Service/Produtos/produtos.service.ts
+++ src/Service/Produtos/produtos.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, NotFoundException, Inject } from '@nestjs/common';
+import { Injectable, NotFoundException, Inject, ConflictException, InternalServerErrorException } from '@nestjs/common';
 import { Produtos } from '../../Database/Models/produtos.entity';
-import { PRODUTOS_REPOSITORY } from 'src/Constants';
+import { DB_ERROR_CATEGORY_NOT_FOUND, MESSAGE_CATEGORY_NOT_FOUND, MESSAGE_INTERNAL_SERVER_ERROR, MESSAGE_PRODUCT_NOT_FOUND, MESSAGE_SEQUELIZE_VALIDATION_ERROR, PRODUTOS_REPOSITORY, SEQUELIZE_VALIDATION_ERROR } from 'src/Constants';
 import { ProdutosDto } from 'src/Module/Produtos/dto/produtos.dto';
 
 @Injectable()
@@ -9,11 +9,29 @@ export class ProdutosService {
     constructor(@Inject(PRODUTOS_REPOSITORY) private readonly produtoRepository: typeof Produtos) { };
 
     async insertProduct(produto: ProdutosDto): Promise<Produtos>{
-        return await this.produtoRepository.create<Produtos>(produto);
+        try{
+            return await this.produtoRepository.create<Produtos>(produto);
+        }
+        catch (error) {
+            const message = error.message;
+
+            if (message === SEQUELIZE_VALIDATION_ERROR)
+                throw new ConflictException(MESSAGE_SEQUELIZE_VALIDATION_ERROR);
+
+            if (message === DB_ERROR_CATEGORY_NOT_FOUND)
+                throw new NotFoundException(MESSAGE_CATEGORY_NOT_FOUND);
+
+            throw new InternalServerErrorException(MESSAGE_INTERNAL_SERVER_ERROR);
+        }
     }
 
     async getSingleProduct(id: number): Promise<Produtos>{
-        return await this.produtoRepository.findOne<Produtos>({ where: { id } });
+        const result = await this.produtoRepository.findOne<Produtos>({ where: { id } });
+
+        if(!result)
+            throw new NotFoundException(MESSAGE_PRODUCT_NOT_FOUND);
+
+        return result;
     }
 
     async getAllProducts(): Promise<Produtos[]>{
@@ -24,7 +42,7 @@ export class ProdutosService {
         const updatedProdutos = await this.produtoRepository.findOne<Produtos>({ where: { id } });
 
         if(!updatedProdutos)
-            throw new NotFoundException('Produto não encontrado');
+            throw new NotFoundException(MESSAGE_PRODUCT_NOT_FOUND);
         
         if(idCategoria)
             updatedProdutos.idCategoria = idCategoria;
@@ -45,14 +63,27 @@ export class ProdutosService {
             updatedProdutos.status = status;
         }
         
-        return await updatedProdutos.save();
+        try{
+            return await updatedProdutos.save();
+        }
+        catch (error) {
+            const message = error.message;
+
+            if (message === SEQUELIZE_VALIDATION_ERROR)
+                throw new ConflictException(MESSAGE_SEQUELIZE_VALIDATION_ERROR);
+
+            if (message === DB_ERROR_CATEGORY_NOT_FOUND)
+                throw new NotFoundException(MESSAGE_CATEGORY_NOT_FOUND);
+
+            throw new InternalServerErrorException(MESSAGE_INTERNAL_SERVER_ERROR);
+        }
     }
 
     async deleteProduct(id: number){
         const produto = await this.produtoRepository.findOne<Produtos>({ where: { id } });
 
         if(!produto)
-            throw new NotFoundException('Produto não encontrado');
+            throw new NotFoundException(MESSAGE_PRODUCT_NOT_FOUND);
         
         return await produto.destroy();
     }
